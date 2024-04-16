29cceb8e88e609d166fb50207cc2fa676a375e8c
unknown <caruidiaz@uninorte.edu.co>
2024-04-15 21:36:49 -0500

    first commit

---
+++ src/core/persona/Cliente.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core.persona;
+
+import core.producto.PlanCliente;
+import core.producto.ProductoCliente;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Carlos Ruidiaz M
+ */
+public class Cliente {
+    
+    private ArrayList<PlanCliente> planes;
+    private ArrayList<ProductoCliente> productos;
+    
+}
